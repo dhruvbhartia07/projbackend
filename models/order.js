6bb3c1175d37b528f6a9b4e4d33d1e7a0c25ba2f
dhruvbhartia07 <dhruvbhartia07@gmail.com>
2022-03-09 17:18:33 +0000

    Added oder schema

---
+++ models/order.js
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+const { ObjectId } = Schema;
+
+const productCartSchema = new Schema({
+    product: {
+        type: ObjectId,
+        ref: "Product"
+    },
+    name: String,
+    count: Number,
+    price: Number,
+
+});
+
+const ProductCart = mongoose.model("ProductCart", productCartSchema);
+
+const orderSchema = new Schema({
+    products: [productCartSchema],
+    transaction_id: {},
+    amount: {
+        type: Number
+    },
+    address: String,
+    update: Date, 
+    user: {
+        type: ObjectId,
+        ref: "User"
+    }
+
+}, {timestamps: true});
+
+
+const Order = mongoose.model("Order", orderSchema);
+
+module.exports = { Order, ProductCart };
