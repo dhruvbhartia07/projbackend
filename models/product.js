7b44496b7c1521d25c2523681c8be08d99b452af
dhruvbhartia07 <dhruvbhartia07@gmail.com>
2022-03-09 16:54:42 +0000

    Added project schema

---
+++ models/product.js
@@ -0,0 +1,44 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+const { ObjectId } = Schema;
+
+const productSchema = new Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: true,
+        maxlength: 32
+    },
+    description: {
+        type: String,
+        trim: true,
+        required: true,
+        maxlength: 2000
+    },
+    price: {
+        type: Number,
+        trim: true,
+        required: true,
+        maxlength: 32
+    },
+    category: {
+        type: ObjectId,
+        ref: "Category",
+        required: true,
+        maxlength: 32
+    },
+    stock: {
+        type: Number
+    },
+    sold: {
+        type: Number,
+        default: 0
+    },
+    photo: {
+        data: Buffer,
+        contentType: String
+    }
+
+}, {timestamp: true});
+
+module.exports = mongoose.model("Product", productSchema);
