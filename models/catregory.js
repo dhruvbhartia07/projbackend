9a8bbdbda5fba334a50cd5fd58449f371ac465bf
dhruvbhartia07 <dhruvbhartia07@gmail.com>
2022-03-09 16:39:39 +0000

    Added Category schema

---
+++ models/catregory.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const categorySchema = new Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: true,
+        maxlength: 32,
+        unique: true
+    }
+}, {timestamp: true});
+
+module.exports = mongoose.model("Category", categorySchema);
