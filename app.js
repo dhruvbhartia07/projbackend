3c139935c28c8820d0d88a51230397fa64242ae3
dhruvbhartia07 <dhruvbhartia07@gmail.com>
2022-03-09 18:37:38 +0000

    Added middlewares

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ require('dotenv').config()
 
 const mongoose = require('mongoose');
 const express = require("express");
-
 const app = express();
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
 
 mongoose.connect(process.env.DATABASE)
   .then(() => {
@@ -13,6 +16,10 @@ mongoose.connect(process.env.DATABASE)
   .catch(
     console.log("DB CONNECTION ERROR!!")
   );
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
 
 const port = process.env.PORT || 8000;
 
