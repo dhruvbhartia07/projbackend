9e25cc28dbfa5873a58590a07cc0a1dcc49c7cb5
dhruvbhartia07 <69341210+dhruvbhartia07@users.noreply.github.com>
2022-03-10 14:35:45 +0530

    Signin route added. Signing in user with checks and validation. If valid creds, creating jwt token and storing in cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,14 @@
 const User = require("../models/user");
 const { validationResult } = require("express-validator");
+var jwt = require("jsonwebtoken");
 
 //Siging up user
 exports.signup = (req, res) => {
-  const user = new User(req.body);
   const err = validationResult(req);
   if (!err.isEmpty()) {
     return res.status(400).json({ err: err.array() });
   }
+  const user = new User(req.body);
   user.save((err, user) => {
     if (err) {
       return res.status(400).json({
@@ -18,6 +19,48 @@ exports.signup = (req, res) => {
       name: user.name,
       email: user.email,
       id: user._id
+    });
+  });
+};
+
+//Signin Logic
+exports.signin = (req, res) => {
+  const { email, password } = req.body;
+
+  const err = validationResult(req);
+  if (!err.isEmpty()) {
+    return res.status(400).json({ err: err.array() });
+  }
+
+  User.findOne({ email }, (err, user) => {
+    if (err || !user) {
+      return res.status(400).json({
+        error: "User does not exists"
+      });
+    }
+
+    if (!user.authenticate(password)) {
+      return res.status(401).json({
+        error: "Email and password do not match"
+      });
+    }
+
+    //Create Token
+    const token = jwt.sign({ _id: user._id }, process.env.SECRET);
+
+    //put token in cokkie
+    res.cookie("token", token, { expire: new Date() + 9999 });
+
+    //send response to frontend
+    const { _id, name, email, role } = user;
+    res.json({
+      token,
+      user: {
+        _id,
+        name,
+        email,
+        role
+      }
     });
   });
 };
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { signup, signout } = require("../controllers/auth");
+const { signup, signin, signout } = require("../controllers/auth");
 const { check } = require("express-validator");
 
 // Defined Signup route
@@ -12,6 +12,15 @@ router.post(
     check("password", "Password should be atleast 3 char").isLength({ min: 3 })
   ],
   signup
+);
+
+router.post(
+  "/signin",
+  [
+    check("email", "Invalid Email").isEmail(),
+    check("password", "Password is required").isLength({ min: 1 })
+  ],
+  signin
 );
 
 // Defined Signout route
