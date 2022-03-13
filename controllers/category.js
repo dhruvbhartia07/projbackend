81fa9451ba3c55debc32514a9e1a5a2ddb0d9eee
dhruvbhartia07 <69341210+dhruvbhartia07@users.noreply.github.com>
2022-03-13 18:18:39 +0530

    Added category routes and controllers

---
+++ controllers/category.js
@@ -0,0 +1,67 @@
+const Category = require("../models/category");
+
+exports.getCategoryById = (req, res, next, id) => {
+  Category.findById(id).exec((err, category) => {
+    if (err) {
+      res.status(400).json({
+        error: "Category not found in DB"
+      });
+    }
+    req.category = category;
+  });
+
+  next();
+};
+
+exports.createCategory = (req, res) => {
+  const category = new Category(req.body);
+  category.save((err, category) => {
+    if (err) {
+      res.status(400).json({
+        error: "Error with Category creation"
+      });
+    }
+    res.json(category);
+  });
+};
+
+exports.getCategory = (req, res) => {
+  res.json(req.category);
+};
+
+exports.getAllCategory = (req, res) => {
+  Category.find().exec((err, categories) => {
+    if (err) {
+      return res.status(400).json("Error fetching categories");
+    }
+    res.json(categories);
+  });
+};
+
+exports.updateCategory = (req, res) => {
+  const category = req.category;
+  category.name = req.body.name;
+
+  category.save((err, updatedCategory) => {
+    if (err) {
+      res.status(400).json({
+        error: "Failed to updated category"
+      });
+    }
+    res.json(updatedCategory);
+  });
+};
+
+exports.removeCategory = (req, res) => {
+  const category = req.category;
+  category.remove((err, category) => {
+    if (err) {
+      res.status(400).json({
+        error: "Failed to delete this category"
+      });
+    }
+    res.json({
+      message: `Successfully deleted ${category} category`
+    });
+  });
+};
